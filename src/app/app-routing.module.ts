2f1b845c7a433160ca7ee030d93f2725d72905e0
Guadalupe Nayeli Leon Ochoa <57240149+Nayelileon@users.noreply.github.com>
2023-05-04 11:48:11 -0700

    Rutas mas o menos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
 
-const routes: Routes = [
-  { path: 'inicio-sesion', component: InicioSesionComponent, pathMatch: "full"},
-  
-];
-
-export const routing = RouterModule.forRoot(routes);
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), BrowserModule],
+  imports: [BrowserModule],
   exports: [RouterModule],
   providers: [],
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Router } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
+
+
+const routes: Routes = [
+
+  { path: 'app.component', component: AppComponent },
+  { path: 'inicio-sesion', component: InicioSesionComponent},
+  { path: 'sidebar', component: SidebarComponent},
+  
+];
+
+export const routing = RouterModule.forRoot(routes);
 
 @NgModule({
   declarations: [
@@ -16,7 +27,7 @@ import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
     InicioSesionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
